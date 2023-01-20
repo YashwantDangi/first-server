39af9d005701d1baefb79709bc38e1f2d4ea033f
YashwantDangi <yashwantdangi65@gmail.com>
2023-01-20 17:36:52 +0530

    server created using express

--- server.js
+++ server.js
@@ -1,86 +1,50 @@
-// server creation
+const express = require("express");
 
-const http = require("http");
+// initialization
+const app = express();
+
+// appln will now use json format for the data transfer
+app.use(express.json());
 
 const port = 8081;
 
 const toDoList = ["Need to learn", "Need to code"];
 
-// http Methods
+// http://localhost:8081/todos
+app.get("/todos", (req, res) => {
+  res.status(200).send(toDoList);
+});
 
-// GET => getting certain details from server / default method / and it can directly work with any browser.
-// PUT => overwrite, fully update.
-// DELETE => deleting data from server.
-// PATCH => update very few files / certain files.
-// POST => sending to the server (like:- Login Credentials).
-
-http
-  .createServer((req, res) => {
-    // res.writeHead(200, { "Content-Type": "text/html" });
-    // res.write("<h1>Hello, Yashwant Singh Dangi new server</h1>");
-    // res.end();
-    const { method, url } = req;
-    // console.log(method, url);
-    // res.end();
-    if (url === "/todos") {
-      // http://localhost:8081/todos
-      if (method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(toDoList.toString());
-      } else if (method === "POST") {
-        let body = "";
-        req
-          .on("error", (err) => {
-            console.log(err);
-          })
-          .on("data", (chunk) => {
-            body += chunk;
-            // console.log(chunk);
-          })
-          .on("end", () => {
-            body = JSON.parse(body);
-            // console.log("body data " , body);
-            let newToDo = toDoList;
-            newToDo.push(body.item);
-            console.log(newToDo);
-            res.writeHead(201);
-          });
-      } else if (method === "DELETE") {
-        let body = "";
-        req
-          .on("error", (err) => {
-            console.error(err);
-          })
-          .on("data", (chunk) => {
-            body += chunk;
-          })
-          .on("end", () => {
-            body = JSON.parse(body);
-            let deleteItem = body.item;
-            for (let i = 0; i < toDoList.length; i++) {
-              if (toDoList[i] === deleteItem) {
-                toDoList.splice(i, 1);
-                break;
-              }
-            }
-            res.writeHead(204);
-          });
-      } else {
-        res.writeHead(501);
-      }
-    } else {
-      res.writeHead(404);
-    }
-    res.end();
-  })
-  .listen(port, () => {
-    console.log(`My NodeJs server started on port ${port}`);
+app.post("/todos", (req,res) => {
+  let newToDoItem = req.body.item;
+  toDoList.push(newToDoItem);
+  res.status(201).send({
+    message: "The to do got added successfully",
   });
+});
 
-// Different routes (url) ->
+app.delete("/todos", (req, res) => {
+  const itemToDelete = req.body.item;
+  toDoList.find((element, index) => {
+    if (element === itemToDelete) {
+      toDoList.splice(index, 1);
+    }
+  });
+  res.status(201).send({
+    message: `Deleted item ${req.body.item}`,
+  });
+});
 
-// To run server : http://localhost:8081
-// http://localhost:8081/
-// http://localhost:8081/home/
-// http://localhost:8081/aboutUs/
-// http://localhost:8081/contactUs/
+
+// General Cases always should be at the bottom
+app.all("/todos", (req, res) => {
+  res.status(501).send();
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send();
+});
+
+app.listen(port, () => {
+  console.log(`Node js server started on ${port}`);
+});
